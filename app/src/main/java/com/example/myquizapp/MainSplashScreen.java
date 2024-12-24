d68eb8a318f691bac6e85e3ee8e638b9927c65f2
mahi <127742360+mahi1974@users.noreply.github.com>
2024-12-24 08:10:20 +0530

    launch screen in progress

--- app/src/main/java/com/example/myquizapp/MainSplashScreen.java
+++ app/src/main/java/com/example/myquizapp/MainSplashScreen.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainSplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_splash_screen);
 
     }
 }
